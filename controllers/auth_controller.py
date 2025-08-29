704a9884c0036f23881cd1d8053fb27d8a8af516
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-08-29 21:45:57 +0700

    add register_user function

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,12 +1,19 @@
 """Module for sending csrf-tokens."""
 
-from typing import Dict
+from typing import Dict, TypedDict
 from flask import jsonify
 from flask_wtf.csrf import generate_csrf
 from google_auth_oauthlib.flow import Flow
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from decouple import config
+
+
+class UserCredentials(TypedDict):
+    """Schema for user credentials."""
+
+    username: str
+    password: str
 
 
 def get_csrf_token():
@@ -18,6 +25,22 @@ def get_csrf_token():
     Returns: A JSON with the csrf-token field.
     """
     return jsonify(csrf_token=generate_csrf())
+
+
+
+def register_user(user_type: str, credentials: UserCredentials, oauth: str =True):
+    """Register a new user using the provided credentials.
+    
+    Create a new credentials in the database based on the provided credentials.
+    If the sign-in method uses oauth, the user's password will be a randomly
+    generated hashstring.
+
+    Args:
+        user_type: The account type
+        credentials: The account's credentials
+    """
+    pass
+
 
 
 def handle_authentication(body: Dict):
@@ -57,6 +80,8 @@ def handle_authentication(body: Dict):
         requests.Request(),
         audience=config("CLIENT_ID", default="very-secret-client-id")
     )
+
+    # put register and login here
 
     return {
         "uid": id_info['sub'],
