ce62b6977c5eda57fb426e7a39070d68df9f6330
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-08-30 00:05:26 +0700

    add login and registration

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -7,6 +7,12 @@ from google_auth_oauthlib.flow import Flow
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from decouple import config
+from db_controller import BaseController
+from jwt import encode
+from datetime import datetime, timedelta, UTC
+
+
+SECRET_KEY = config("SECRET_KEY", default="good-key123")
 
 
 class UserCredentials(TypedDict):
@@ -14,6 +20,7 @@ class UserCredentials(TypedDict):
 
     username: str
     password: str
+    user_type: str
 
 
 def get_csrf_token():
@@ -25,22 +32,6 @@ def get_csrf_token():
     Returns: A JSON with the csrf-token field.
     """
     return jsonify(csrf_token=generate_csrf())
-
-
-
-def register_user(user_type: str, credentials: UserCredentials, oauth: str =True):
-    """Register a new user using the provided credentials.
-    
-    Create a new credentials in the database based on the provided credentials.
-    If the sign-in method uses oauth, the user's password will be a randomly
-    generated hashstring.
-
-    Args:
-        user_type: The account type
-        credentials: The account's credentials
-    """
-    pass
-
 
 
 def handle_authentication(body: Dict):
@@ -81,10 +72,68 @@ def handle_authentication(body: Dict):
         audience=config("CLIENT_ID", default="very-secret-client-id")
     )
 
-    # put register and login here
+    auth_controller = AuthenticationController()
 
-    return {
-        "uid": id_info['sub'],
-        "access_token": credentials.token,
-        "refresh_token": credentials.refresh_token,
-    }
+    is_registered = auth_controller.check_users(id_info['sub'])
+
+    user_jwt = {}
+    if is_registered:
+        user_jwt = auth_controller.login_user(id_info['sub'])
+    else:
+        auth_controller.register_user(id_info['email'], id_info['at_hash'], 'company')
+        user_jwt = auth_controller.login_user(id_info['sub'])
+
+    return user_jwt
+
+
+class AuthenticationController(BaseController):
+    """Controller for fetching database authentication credentials."""
+
+    def __init__(self):
+        """Init the class."""
+        super().__init__()
+
+    def check_users(self, google_id: str):
+        """Check if the user is in the users table."""
+        query = "SELECT * FROM user_google_auth_info;"
+        result = self.execute_query(query, fetchall=True)
+        
+        if google_id in [row for row in result['google_uid']]: 
+            return True
+        return False
+    
+    def login_user(self, uid: str) -> Dict[str, str]:
+        """Return a JTW for authorization."""
+        refresh = {'iat': datetime.now(UTC),
+                   'exp': datetime.now(UTC) + timedelta(hours=1)}
+
+        payload = {'uid': uid,
+                   'refresh_token': refresh
+                   }
+        
+        auth_token = encode(payload, SECRET_KEY, algorithm="HS512")
+
+        return {"user_token" : auth_token}
+
+
+    def register_user(self, credentials: UserCredentials):
+        """Register a new user using the provided credentials.
+        
+        Create a new credentials in the database based on the provided credentials.
+
+        Args:
+            credentials: The account's credentials
+        """
+        keys = ["username", "password", "user_type"]
+        valid_keys = all(key in credentials for key in keys)
+        if not valid_keys:
+            raise TypeError("Invalid credentials.")
+
+        query = f"""
+                INSERT INTO users (username, password, user_type) 
+                VALUES ('{credentials['username']}',
+                        '{credentials['password']}',
+                        '{credentials['user_type']}');
+                """
+        return self.execute_query(query, commit=True)
+
