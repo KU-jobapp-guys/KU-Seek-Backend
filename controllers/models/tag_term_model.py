d781d82af36eced44368a319f03ac95501861fb4
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2025-09-14 07:54:29 +0700

    feat: add tag term model

---
+++ controllers/models/tag_term_model.py
@@ -0,0 +1,24 @@
+"""Module for Tag and Term tables."""
+
+from .base_model import BaseModel
+from sqlalchemy.orm import Mapped, MappedColumn
+from sqlalchemy import String, Integer
+
+
+class Tags(BaseModel):
+    """Tags model."""
+
+    __tablename__ = "tags"
+
+    id: Mapped[int] = MappedColumn(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = MappedColumn(String(40), nullable=True)
+
+
+class Terms(BaseModel):
+    """Terms model."""
+
+    __tablename__ = "terms"
+    
+    id: Mapped[int] = MappedColumn(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = MappedColumn(String(40), nullable=True)
+    type: Mapped[str] = MappedColumn(String(40), nullable=True)
