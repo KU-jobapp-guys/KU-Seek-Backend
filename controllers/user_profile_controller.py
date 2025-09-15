a2357dfcf664e0b8c822b38955abdc4b0ae9aa64
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2025-09-16 00:53:19 +0700

    chore: add session to update_profile

--- controllers/user_profile_controller.py
+++ controllers/user_profile_controller.py
@@ -1,6 +1,7 @@
 """Module for store api that relate to user profile."""
 
 from .db_controller import BaseController
+from .models.profile_model import Profile
 
 class ProfileController(BaseController):
     """Controller to use CRUD operations for UserProfile."""
@@ -17,5 +18,12 @@ class ProfileController(BaseController):
         Update user profile in the MySQL database.
         Corresponds to: PATCH /users/{user_id}/profile
         """
+        update_fields = [(key, val) for key, val in body.items()]
+
+        session = self.get_session()
+
+        session.commit()
+        session.close()
+
         return {}
     
