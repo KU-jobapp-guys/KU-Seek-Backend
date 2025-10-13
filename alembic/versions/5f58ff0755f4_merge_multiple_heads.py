69bd144b476152a8a3157e060ed34fc8177ce859
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-13 19:20:29 +0700

    resolve migration conflict

--- alembic/versions/5f58ff0755f4_merge_multiple_heads.py
+++ alembic/versions/5f58ff0755f4_merge_multiple_heads.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '5f58ff0755f4'
-down_revision: Union[str, Sequence[str], None] = ('16882f9cb8bf', '3ce7adcbc68f')
+down_revision: Union[str, Sequence[str], None] = '3ce7adcbc68f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
