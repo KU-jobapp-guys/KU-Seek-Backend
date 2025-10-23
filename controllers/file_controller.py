914c1f1fe2cdd8c8c51ced269e415daffe7d9c3e
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-23 22:51:10 +0700

    update method docstrings

--- controllers/file_controller.py
+++ controllers/file_controller.py
@@ -11,6 +11,7 @@ from .models.file_model import File
 
 FILE_DIR = config("BASE_FILE_PATH", default="content")
 
+
 class FileController:
     """Class for handling file serving."""
 
@@ -21,7 +22,17 @@ class FileController:
 
     @login_required
     def get_file(self, file_id: str):
-        """Return a file for viewing in the browser."""
+        """
+        Return a file for viewing in the browser.
+
+        Serve a file to the frontend, with the provided file id.
+        This method requires authentication.
+
+        Args:
+            file_id: The file's UID
+
+        Returns A flask response object, containing the file.
+        """
         try:
             session = self.db.get_session()
             file = session.query(File).where(File.id == file_id).one_or_none()
@@ -41,7 +52,17 @@ class FileController:
 
     @login_required
     def download_file(self, file_id: str):
-        """Return a file for downloading."""
+        """
+        Return a file for downloading.
+
+        Serve a file to the frontend as an attachment, with the provided file id.
+        This method requires authentication.
+
+        Args:
+            file_id: The file's UID
+
+        Returns A flask response object, containing the file as an attachment.
+        """
         try:
             session = self.db.get_session()
             file = session.query(File).where(File.id == file_id).one_or_none()
