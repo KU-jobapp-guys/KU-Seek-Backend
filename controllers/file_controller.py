60d7c7ef98ad57ab7a94154eaf0a5d532dd4f2fc
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-23 22:48:01 +0700

    implement file return logic for get_file and download_file

--- controllers/file_controller.py
+++ controllers/file_controller.py
@@ -1,32 +1,60 @@
 """Module containing endpoints for file serving."""
 
 import os
-from flask import send_file
-from uuid import UUID
-from jwt import decode
-from .decorators import role_required
-from flask import request
-from decouple import config, Csv
-from sqlalchemy.orm import joinedload
+
+from flask import send_from_directory
+from .decorators import login_required
+from decouple import config
 from swagger_server.openapi_server import models
-from werkzeug.utils import secure_filename
-from .models.job_model import Job, JobApplication
-from .models.user_model import Student, Company
 from .models.file_model import File
 
 
+FILE_DIR = config("BASE_FILE_PATH", default="content")
 
 class FileController:
     """Class for handling file serving."""
 
-    def __init__(self):
+    def __init__(self, database):
         """Initialize the class."""
-        self.base_path = config("BASE_FILE_PATH", default="content")
+        self.db = database
+        self.base_path = os.path.join(os.getcwd(), FILE_DIR)
 
-    def get_file(self, file_id:str):
+    @login_required
+    def get_file(self, file_id: str):
         """Return a file for viewing in the browser."""
-        pass
+        try:
+            session = self.db.get_session()
+            file = session.query(File).where(File.id == file_id).one_or_none()
+            if not file:
+                session.close()
+                return models.ErrorMessage("File record not found"), 404
+            file_name = file.file_name
+            session.close()
+        except Exception as e:
+            session.close()
+            return models.ErrorMessage("Database Error occured: ", e), 400
 
-    def download_file(self, file_id:str):
+        try:
+            return send_from_directory(self.base_path, file_name, as_attachment=False)
+        except FileNotFoundError:
+            return models.ErrorMessage("File not found"), 404
+
+    @login_required
+    def download_file(self, file_id: str):
         """Return a file for downloading."""
-        pass
+        try:
+            session = self.db.get_session()
+            file = session.query(File).where(File.id == file_id).one_or_none()
+            if not file:
+                session.close()
+                return models.ErrorMessage("File record not found"), 404
+            file_name = file.file_name
+            session.close()
+        except Exception as e:
+            session.close()
+            return models.ErrorMessage("Database Error occured: ", e), 400
+
+        try:
+            return send_from_directory(self.base_path, file_name, as_attachment=True)
+        except FileNotFoundError:
+            return models.ErrorMessage("File not found"), 404
