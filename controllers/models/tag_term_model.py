770eb687a340e03aa81cd4f056ced32346f501b7
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2025-10-26 22:44:45 +0700

    chore: update annoucement schema to match the frontend requirement

--- controllers/models/tag_term_model.py
+++ controllers/models/tag_term_model.py
@@ -22,3 +22,4 @@ class Terms(BaseModel):
     id: Mapped[int] = MappedColumn(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = MappedColumn(String(40), nullable=True)
     type: Mapped[str] = MappedColumn(String(40), nullable=True)
+
