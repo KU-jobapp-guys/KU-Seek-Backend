3ea25b78c82e76a9890170299673300cf8962211
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-11-02 15:21:51 +0700

    refactor email files into email folder

--- controllers/management/email/email_sender.py
+++ controllers/management/email/email_sender.py
@@ -3,32 +3,52 @@
 import smtplib
 import os
 
+from abc import ABC, abstractmethod
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from decouple import config
+
 
 SERVER_EMAIL = config("SERVICE_EMAIL", default="faker@gmail.com")
 EMAIL_PW = config("EMAIL_PASSWORD", default="realpw123")
 
 
-class EmailSender:
-    """Class for sending emails via SMTP."""
+class EmailStrategy(ABC):
+    """Abstract class for defining email sending strategies."""
+
+    @abstractmethod
+    def send_email(self, recipient: str, topic: str, email_file: str):
+        """Send an email to a recipient."""
+        raise NotImplementedError
+
+
+class GmailEmailStrategy(EmailStrategy):
+    """Class for sending emails via Gmail's SMTP."""
 
     def __init__(self):
         """Initialize the class."""
         self.email = SERVER_EMAIL
 
-    def send_email(self, recipient: str, topic: str, email_file: str):
+    def send_email(
+        self,
+        recipient: str,
+        topic: str,
+        email_file: str,
+        template_args: list[tuple[str, str]] = [],
+    ):
         """
         Send an email to the recipient.
 
-        Sends an email to the provided recipient with the provided topic and body.
+        Sends an email to the provided recipient with the provided topic and
+        template body contained in email_file.
 
         Args:
             recipient: The recipient's email
             topic: The topic/subject to address the email with
             email_file: The file name for the email's body/content
                         without the file extension
+            template_args: Tuple containing the template to replace and its value,
+                           example: ("{{recipient}}", "faker@gmail.com")
         """
         msg = MIMEMultipart("alternative")
         msg["Subject"] = topic
@@ -43,6 +63,8 @@ class EmailSender:
             with open(email_file + ".html", "r", encoding="utf-8") as f:
                 html_content = f.read()
                 html_content = html_content.replace("{{UserName}}", recipient)
+                for arg in template_args:
+                    html_content = html_content.replace(arg[0], arg[1])
         except IOError as e:
             raise IOError("HTML file could not be read.", e)
 
@@ -50,6 +72,8 @@ class EmailSender:
             with open(email_file + ".txt", "r", encoding="utf-8") as f:
                 text_content = f.read()
                 text_content = text_content.replace("{{UserName}}", recipient)
+                for arg in template_args:
+                    html_content = html_content.replace(arg[0], arg[1])
         except IOError as e:
             raise IOError("Plain text file could not be read.", e)
 
@@ -72,3 +96,21 @@ class EmailSender:
                 smtp_server.quit()
         except Exception as e:
             raise ValueError("Email could not be sent.", e)
+
+
+class EmailSender:
+    """Class for sending Emails."""
+
+    def __init__(self, strategy: EmailStrategy):
+        """Initialize the class."""
+        self.strategy = strategy
+
+    def send_email(
+        self,
+        recipient: str,
+        topic: str,
+        email: str,
+        template_args: list[tuple[str, str]],
+    ):
+        """Send an email to the recipient."""
+        self.strategy.send(recipient, topic, email, template_args)
