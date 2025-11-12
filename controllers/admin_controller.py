1c9b4eb4fb19c88c23f8ec226232a6b5d0031a02
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-11-12 20:43:32 +0700

    implement get user request records

--- controllers/admin_controller.py
+++ controllers/admin_controller.py
@@ -1,15 +1,20 @@
 """Module containing admin endpoints."""
-from decorators import role_required
+
+from .decorators import role_required
+from db_controller import AbstractDatabaseController
+from .models.admin_request_model import UserRequest, JobRequest, RequestStatusTypes
+from .models.job_model import Job
+from .models.profile_model import Profile
 
 
-@role_required(["Admin"])
 class AdminController:
     """Class for handling admin operations."""
 
     def __init__(self, database):
         """Initialize the class."""
-        self.db = database
+        self.db: AbstractDatabaseController = database
 
+    @role_required(["Admin"])
     def get_all_user_request(self):
         """
         Get all user creation requests.
@@ -19,8 +24,19 @@ class AdminController:
 
         returns: All non-resolved user creation records.
         """
-        pass
+        session = self.db.get_session()
+        user_requests = (
+            session.query(UserRequest, Profile.first_name, Profile.last_name)
+            .join(Profile, Profile.user_id == UserRequest.user_id)
+            .where(UserRequest.status != RequestStatusTypes.DENIED.value)
+            .all()
+        )
+        records = [record.to_dict() for record in user_requests]
+        session.close()
 
+        return records, 200
+
+    @role_required(["Admin"])
     def update_user_status(self, body):
         """
         Update one or more user verification statuses.
@@ -34,6 +50,7 @@ class AdminController:
         """
         pass
 
+    @role_required(["Admin"])
     def get_all_job_request(self):
         """
         Get all job post creation requests.
@@ -44,6 +61,7 @@ class AdminController:
         """
         pass
 
+    @role_required(["Admin"])
     def update_job_status(self, body):
         """
         Update one or more job post verification statuses.
