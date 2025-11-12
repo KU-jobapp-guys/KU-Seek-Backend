e93ed8d48ce2bd7cc99df4093691dd5a26594890
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-11-12 20:43:11 +0700

    ruff lint

--- controllers/models/admin_request_model.py
+++ controllers/models/admin_request_model.py
@@ -6,8 +6,9 @@ from datetime import datetime
 from .base_model import BaseModel
 from sqlalchemy.orm import Mapped, MappedColumn
 from sqlalchemy import ForeignKey, DateTime, func
-from sqlalchemy.types import Enum, String
+from sqlalchemy.types import Enum
 from .user_model import UserTypes
+from .profile_model import Profile
 
 
 class RequestStatusTypes(enum.Enum):
@@ -18,23 +19,25 @@ class RequestStatusTypes(enum.Enum):
     DENIED = "denied"
 
 
-
 class UserRequest(BaseModel):
     """User creation request model."""
 
     __tablename__ = "user_requests"
     id = Mapped[int] = MappedColumn(primary_key=True, autoincrement=True)
-    user_id: Mapped[uuid.UUID] = MappedColumn(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    user_id: Mapped[uuid.UUID] = MappedColumn(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
     requested_type: Mapped[Enum] = MappedColumn(Enum(UserTypes))
-    status: Mapped[Enum] = MappedColumn(Enum(RequestStatusTypes), default=RequestStatusTypes.PENDING)
+    status: Mapped[Enum] = MappedColumn(
+        Enum(RequestStatusTypes), default=RequestStatusTypes.PENDING
+    )
     created_at: Mapped[datetime] = MappedColumn(
         DateTime, default=func.now(), nullable=False
     )
-    approved_at: Mapped[datetime] = MappedColumn(
-        DateTime, nullable=True
+    approved_at: Mapped[datetime] = MappedColumn(DateTime, nullable=True)
+    approved_by: Mapped[uuid.UUID] = MappedColumn(
+        ForeignKey("users.id", ondelete="SET NULL"), nullable=False
     )
-    approved_by: Mapped[uuid.UUID] = MappedColumn(ForeignKey("users.id", ondelete="SET NULL"), nullable=False)
-
 
 
 class JobRequest(BaseModel):
@@ -42,12 +45,16 @@ class JobRequest(BaseModel):
 
     __tablename__ = "job_requests"
     id = Mapped[int] = MappedColumn(primary_key=True, autoincrement=True)
-    job_id: Mapped[uuid.UUID] = MappedColumn(ForeignKey("jobs.id", ondelete="CASCADE"), nullable=False)
-    status: Mapped[Enum] = MappedColumn(Enum(RequestStatusTypes), default=RequestStatusTypes.PENDING)
+    job_id: Mapped[uuid.UUID] = MappedColumn(
+        ForeignKey("jobs.id", ondelete="CASCADE"), nullable=False
+    )
+    status: Mapped[Enum] = MappedColumn(
+        Enum(RequestStatusTypes), default=RequestStatusTypes.PENDING
+    )
     created_at: Mapped[datetime] = MappedColumn(
         DateTime, default=func.now(), nullable=False
     )
-    approved_at: Mapped[datetime] = MappedColumn(
-        DateTime, nullable=True
+    approved_at: Mapped[datetime] = MappedColumn(DateTime, nullable=True)
+    approved_by: Mapped[uuid.UUID] = MappedColumn(
+        ForeignKey("users.id", ondelete="SET NULL"), nullable=False
     )
-    approved_by: Mapped[uuid.UUID] = MappedColumn(ForeignKey("users.id", ondelete="SET NULL"), nullable=False)
